8e4c7a40e4869dd884a2026647cf513711fb585a
Miguel Retroz <contato.miguelretroz@gmail.com>
2022-01-23 05:44:53 -0300

    Conditional structure exercises-> Exercise 04

---
+++ section-03-logic-with-c#/ConditionalStructureExercises/Exercise04/Exercise04/Program.cs
@@ -0,0 +1,19 @@
+﻿string[] inputLine = Console.ReadLine().Split(' ');
+int startTime = int.Parse(inputLine[0]);
+int endTime = int.Parse(inputLine[1]);
+
+Console.WriteLine();
+
+int duration = 0;
+
+if (startTime == endTime)
+    duration = 24;
+else if (startTime > endTime)
+{
+    duration = (24 - startTime) + endTime;
+} else
+{
+    duration = endTime - startTime;
+}
+
+Console.WriteLine("O jogo durou {0} hora(s)", duration);
