d1d511bea10869e84347868b31803cf6b9da39bf
Miguel Retroz <contato.miguelretroz@gmail.com>
2022-01-23 09:22:28 -0300

    Repetitive structure FOR exercises-> Exercise 04

---
+++ section-03-logic-with-c#/RepetitiveStructureExercises/For/Exercise04/Exercise04/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i += 1)
+{
+    string[] inputLine = Console.ReadLine().Split(' ');
+    int x = int.Parse(inputLine[0]);
+    int y = int.Parse(inputLine[1]);
+
+    if (y == 0)
+        Console.WriteLine("Divisão impossível");
+    else
+    {
+        double result = (double)x / y;
+        Console.WriteLine(result.ToString("F1", CultureInfo.InvariantCulture));
+    }
+}
