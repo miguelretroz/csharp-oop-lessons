3ec219736520065adbc7d91d51bd6eaafceeec19
Miguel Retroz <contato.miguelretroz@gmail.com>
2022-01-23 07:38:46 -0300

    Functions

---
+++ section-03-logic-with-c#/Functions/Functions/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Digite três números:");
+int n1 = int.Parse(Console.ReadLine());
+int n2 = int.Parse(Console.ReadLine());
+int n3 = int.Parse(Console.ReadLine());
+
+Console.WriteLine();
+
+int result = Greater(n1, n2, n3);
+
+Console.WriteLine("Maior {0}", result);
+
+static int Greater(int numberOne, int numberTwo, int numberThree)
+{
+    int greater;
+
+    if (numberOne > numberTwo && numberOne > numberThree)
+        greater = numberOne;
+    else if (numberTwo > numberThree)
+        greater = numberTwo;
+    else
+        greater = numberThree;
+
+    return greater;
+}
