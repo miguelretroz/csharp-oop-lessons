ff6477afa8cc71111cd7afd5ba969777923f3d79
Miguel Retroz <contato.miguelretroz@gmail.com>
2022-01-23 05:46:18 -0300

    Conditional structure exercises-> Exercise 07

---
+++ section-03-logic-with-c#/ConditionalStructureExercises/Exercise07/Exercise07/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+
+string[] inputLine = Console.ReadLine().Split(' ');
+double x = double.Parse(inputLine[0], CultureInfo.InvariantCulture);
+double y = double.Parse(inputLine[1], CultureInfo.InvariantCulture);
+
+if (x == y && y == 0)
+    Console.WriteLine("Origem");
+else if (x == 0)
+    Console.WriteLine("Eixo X");
+else if (y == 0)
+    Console.WriteLine("Eixo Y");
+else if (x > 0)
+{
+    if (y > 0)
+        Console.WriteLine("Q1");
+    else
+        Console.WriteLine("Q4");
+}
+else
+{
+    if (y > 0)
+        Console.WriteLine("Q2");
+    else
+        Console.WriteLine("Q3");
+}
