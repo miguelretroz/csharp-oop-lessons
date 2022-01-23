581aac01de8597246c44ac66d57f71c89d271457
Miguel Retroz <contato.miguelretroz@gmail.com>
2022-01-23 08:26:21 -0300

    Repetitive structure exercises-> Exercises 03

---
+++ section-03-logic-with-c#/RepetitiveStructureExercises/Exercise03/Exercise03/Program.cs
@@ -0,0 +1,25 @@
+﻿int alcohol = 0, gasoline = 0, diesel = 0, code;
+
+code = int.Parse(Console.ReadLine());
+
+while(code != 4)
+{
+    if (code == 1)
+    {
+        alcohol++;
+    }
+    else if (code == 2)
+    {
+        gasoline++;
+    }
+    else if (code == 3)
+    {
+        diesel++;
+    }
+    code = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("MUITO OBRIGADO!");
+Console.WriteLine("Álcool {0}", alcohol);
+Console.WriteLine("Gasolina {0}", gasoline);
+Console.WriteLine("Diesel {0}", diesel);
