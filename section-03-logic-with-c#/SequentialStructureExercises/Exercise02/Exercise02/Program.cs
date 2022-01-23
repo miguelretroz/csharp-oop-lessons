0abbb4f25bb5f062c3517fd1245b474a0117a83d
Miguel Retroz <contato.miguelretroz@gmail.com>
2022-01-23 04:02:46 -0300

    Sequential structure exercise-> Exercise 02

---
+++ section-03-logic-with-c#/SequentialStructureExercises/Exercise02/Exercise02/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Globalization;
+
+Console.Write("Entre com o raio do circulo: ");
+double radius = double.Parse(Console.ReadLine());
+
+Console.WriteLine();
+
+double area = Math.PI * (Math.Pow(radius, 2.0));
+
+Console.WriteLine("A=" + area.ToString("F4", CultureInfo.InvariantCulture));
