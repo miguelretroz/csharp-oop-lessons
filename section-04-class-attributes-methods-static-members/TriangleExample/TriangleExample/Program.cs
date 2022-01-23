d3a3e031ce8b2fcfc9f9bc865284bd63fd685d71
Miguel Retroz <contato.miguelretroz@gmail.com>
2022-01-23 14:27:39 -0300

    Triangle example-> Add method

--- section-04-class-attributes-methods-static-members/TriangleExample/TriangleExample/Program.cs
+++ section-04-class-attributes-methods-static-members/TriangleExample/TriangleExample/Program.cs
@@ -16,11 +16,9 @@ y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-double p = (x.A + x.B + x.C) / 2.0;
-double xArea = Math.Sqrt(p * (p - x.A) * (p - x.B) * (p - x.C));
+double xArea = x.Area();
 
-p = (y.A + y.B + y.C) / 2.0;
-double yArea = Math.Sqrt(p * (p - y.A) * (p - y.B) * (p - y.C));
+double yArea = y.Area();
 
 Console.WriteLine("Área de X = " + xArea.ToString("F4", CultureInfo.InvariantCulture));
 Console.WriteLine("Área de Y = " + yArea.ToString("F4", CultureInfo.InvariantCulture));
--- section-04-class-attributes-methods-static-members/TriangleExample/TriangleExample/Triangle.cs
+++ section-04-class-attributes-methods-static-members/TriangleExample/TriangleExample/Triangle.cs
@@ -1,9 +1,17 @@
-﻿namespace TriangleExample
+﻿using System;
+
+namespace TriangleExample
 {
     internal class Triangle
     {
         public double A;
         public double B;
         public double C;
+
+        public double Area()
+        {
+            double p = (A + B + C) / 2.0;
+            return Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+        }
     }
 }
