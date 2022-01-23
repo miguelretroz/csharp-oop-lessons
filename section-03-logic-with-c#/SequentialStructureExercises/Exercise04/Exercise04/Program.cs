deba8035d7345b83eacadcae29d2032fc5408d93
Miguel Retroz <contato.miguelretroz@gmail.com>
2022-01-23 04:04:44 -0300

    Sequential structure exercise-> Exercise 04

---
+++ section-03-logic-with-c#/SequentialStructureExercises/Exercise04/Exercise04/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Globalization;
+
+Console.Write("Número do funcionário: ");
+int employeeNumber = int.Parse(Console.ReadLine());
+
+Console.Write("Horas trabalhadas: ");
+int hoursWorked = int.Parse(Console.ReadLine());
+
+Console.Write("Valor por hora: ");
+double hourlyRate = double.Parse(Console.ReadLine());
+
+double salary = hourlyRate * hoursWorked;
+
+Console.WriteLine($"NUMBER = {employeeNumber}");
+Console.WriteLine("SALARY = U$ " + salary.ToString("F2", CultureInfo.InvariantCulture));
