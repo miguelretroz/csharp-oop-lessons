ed96be58c9c9b73715dedf7d8e5e75f7cb726a04
Miguel Retroz <contato.miguelretroz@gmail.com>
2022-01-23 13:59:33 -0300

    Class exercises-> Exercise 01

---
+++ section-04-class-attributes-methods-static-members/ClassExercises/Exercise01/Exercise01/Person.cs
@@ -0,0 +1,8 @@
+﻿namespace Exercise01
+{
+    internal class Person
+    {
+        public string Name;
+        public int Age;
+    }
+}
---
+++ section-04-class-attributes-methods-static-members/ClassExercises/Exercise01/Exercise01/Program.cs
@@ -0,0 +1,29 @@
+﻿using Exercise01;
+
+Person personOne, personTwo;
+
+personOne = new Person();
+personTwo = new Person();
+
+Console.WriteLine("Dados da primeira pessoa:");
+Console.Write("Nome: ");
+personOne.Name = Console.ReadLine();
+Console.Write("Idade: ");
+personOne.Age = int.Parse(Console.ReadLine());
+
+
+Console.WriteLine();
+
+Console.WriteLine("Dados da segunda pessoa:");
+Console.Write("Nome: ");
+personTwo.Name = Console.ReadLine();
+Console.Write("Idade: ");
+personTwo.Age = int.Parse(Console.ReadLine());
+
+Console.WriteLine();
+
+
+if (personOne.Age > personTwo.Age)
+    Console.WriteLine("Pessoa mais velha: {0}", personOne.Name);
+else
+    Console.WriteLine("Pessoa mais velha: {0}", personTwo.Name);
