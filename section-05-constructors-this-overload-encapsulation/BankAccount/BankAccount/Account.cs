b2d70a29fef77593285a549786c867f6c2afd32f
Miguel Retroz <contato.miguelretroz@gmail.com>
2022-01-24 08:56:47 -0300

    Bank account exercise

---
+++ section-05-constructors-this-overload-encapsulation/BankAccount/BankAccount/Account.cs
@@ -0,0 +1,42 @@
+﻿using System.Globalization;
+
+namespace BankAccount
+{
+    internal class Account
+    {
+        public int Number { get; private set; }
+        public string OwnerName { get; set; }
+        public double Amount { get; private set; }
+
+        public Account(int number, string ownerName)
+        {
+            Number = number;
+            OwnerName = ownerName;
+        }
+
+        public Account(int number, string ownerName, double initialAmount) : this(number, ownerName)
+        {
+            Deposit(initialAmount);
+        }
+
+        public void Deposit(double depositAmount)
+        {
+            Amount += depositAmount;
+        }
+
+        public void Withdraw(double withdrawAmount)
+        {
+            Amount -= withdrawAmount + 5.00;
+        }
+
+        public override string ToString()
+        {
+            return "Conta "
+                + Number
+                + ", Titular: "
+                + OwnerName
+                + ", Saldo: $ "
+                + Amount.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
