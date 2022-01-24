ed9ca0920365095b5aa8543afd1cade4d73e2cf1
Miguel Retroz <contato.miguelretroz@gmail.com>
2022-01-24 04:22:57 -0300

    Class exercises 02-> Exericse 03

---
+++ section-04-class-attributes-methods-static-members/ClassExercises02/Exercise03/Exercise03/Student.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Exercise03
+{
+    internal class Student
+    {
+        public string Name;
+        public double GradeOne, GradeTwo, GradeThree;
+
+        public double FinalGrade()
+        {
+            return GradeOne + GradeTwo + GradeThree;
+        }
+
+        public string IsApproved()
+        {
+            double finalGrade = FinalGrade();
+
+            if (finalGrade < 60)
+                return "REPROVADO\nFALTARAM "
+                    + (60.0 - finalGrade).ToString("F2", CultureInfo.InvariantCulture)
+                    + " PONTOS";
+            else
+                return "APROVADO";
+        }
+
+        public override string ToString()
+        {
+            return "NOTA FINAL = "
+                + FinalGrade().ToString("F2", CultureInfo.InvariantCulture)
+                + "\n"
+                + IsApproved();
+        }
+    }
+}
