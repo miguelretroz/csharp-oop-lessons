7b6aa5443facb2fd891c584bbd8f4465d6f36483
Miguel Retroz <contato.miguelretroz@gmail.com>
2022-01-24 06:20:36 -0300

    Product example-> Constructors

---
+++ section-05-constructors-this-overload-encapsulation/ProductExample/ProductExample/Product.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace ProductExample
+{
+    internal class Product
+    {
+        public string Name;
+        public double Price;
+        public int Quantity;
+
+        public Product(string name, double price, int quantity)
+        {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+        public double TotalValueInStock()
+        {
+            return Price * Quantity;
+        }
+        public void AddProducts(int quantidade)
+        {
+            Quantity += quantidade;
+        }
+        public void RemoveProducts(int quantidade)
+        {
+            Quantity -= quantidade;
+        }
+        public override string ToString()
+        {
+            return Name
+            + ", $ "
+            + Price.ToString("F2", CultureInfo.InvariantCulture)
+            + ", "
+            + Quantity
+            + " unidades, Total: $ "
+            + TotalValueInStock().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
