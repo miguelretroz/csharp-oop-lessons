020bd8a582e586d63f3d67b3968edec57eb5fea6
Miguel Retroz <contato.miguelretroz@gmail.com>
2022-01-24 03:46:17 -0300

    Class exercises 02-> Exercise 02

---
+++ section-04-class-attributes-methods-static-members/ClassExercises02/Exercise02/Exercise02/Employee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Exercise02
+{
+    internal class Employee
+    {
+        public string Name;
+        public double GrossSalary;
+        public double Tax;
+
+        public double NetSalary()
+        {
+            return GrossSalary - Tax;
+        }
+
+        public void IncreaseSalary(double percentage)
+        {
+            GrossSalary += GrossSalary * (percentage / 100);
+        }
+
+        public override string ToString()
+        {
+            return Name
+                + ", $ "
+                + NetSalary().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
