767f7045ed4e9a9a6775474efd349131d546f071
Miguel Retroz <contato.miguelretroz@gmail.com>
2022-01-24 03:17:19 -0300

    Class exercises 02-> Exercise 01

---
+++ section-04-class-attributes-methods-static-members/ClassExercises02/Exercise01/Exercise01/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Globalization;
+using Exercise01;
+
+Rectangle rectangle = new Rectangle();
+
+Console.WriteLine("Entre a largura e altura do retângulo:");
+rectangle.Width = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+rectangle.Height = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Console.WriteLine();
+Console.WriteLine(rectangle);
